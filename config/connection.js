4ed86dfee945dc7e320461df1257818d9504a891
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-01-11 19:54:42 -0500

    adding .env

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,15 @@
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
-const sequelize = new Sequelize('ecommerce_db', 'root', 'karli.2606', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
 module.exports = sequelize;
